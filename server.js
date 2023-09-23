80b3270b426fc78a3924caa5809e45d3da1a3b47
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-09-23 18:16:22 +0530

    crud

---
+++ server.js
@@ -0,0 +1,28 @@
+// Import the Express framework
+import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/UserRoute.js";
+// Create an instance of the Express application
+const app = express();
+// Middleware to parse JSON data in incoming requests
+app.use(express.json());
+
+/* MONGOOSE SETUP */
+// Define a different port for the server to listen on if the environment variable 'PORT' is set
+const PORT = 3000 || 9000;
+// Import and connect to MongoDB using Mongoose
+mongoose
+  .connect(
+    "mongodb+srv://Priyanka:srcn1224@cluster0.mobsisi.mongodb.net/?retryWrites=true&w=majority"
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+  )
+  .then(() => {
+    // Start the Express server and listen on the specified port
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}, DB connected`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+app.use("/users", UserRoutes);
